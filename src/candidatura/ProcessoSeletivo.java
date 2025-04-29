4078490f6988b6efbee06bae752ff61c812b4521
elias-mb3 <monteiroelias783@gmail.com>
2025-04-29 18:29:19 -0300

    feat: :sparkles: Selecionando candidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,38 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        analisarCandidato(1500.0);
-        analisarCandidato(2000.0);
-        analisarCandidato(3000.0);
+        selecaoCandidatos();
+    }
+
+    static void selecaoCandidatos(){
+        String[] candidatos = {"João", "Maria", "José", "Ana", "Pedro", "Lucas", "Fernanda", "Carla", "Roberto", "Patrícia"};
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 5) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+            System.out.println("Candidato: " + candidato + ", Solicitou este valor de salário: " + salarioPretendido);
+            if(salarioBase >= salarioPretendido) {
+                System.out.println("Candidato " + candidato + " selecionado!");
+                candidatosSelecionados++;
+            } else {
+                System.out.println("Candidato " + candidato + " não selecionado!");
+            }
+            candidatoAtual++;
+            if (candidatoAtual >= candidatos.length) {
+                System.out.println("Todos os candidatos foram avaliados!");
+                break;
+            }
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800.0, 2200.0);
     }
 
     static void analisarCandidato(double salarioPretendido) {
